6964e62974208ee1c6b75705aee8bad047196975
khadam ikhwanus shofa <33866110+kampretosjr@users.noreply.github.com>
2019-07-17 18:39:45 +0700

    Update routes.js
    update cors

--- src/route/routes.js
+++ src/route/routes.js
@@ -1,18 +1,26 @@
 module.exports = (app) => {
     const controller = require('../controller/controller')
 
+    //kodingan cors anjing yang bikin error
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin","*");
+      res.header("Access-Control-Allow-Methods","GET,POST,PUT,PATCH,DELETE,COPY,HEAD,OPTIONS,LINK,UNLINK,PURGE,LOCK,UNLOCK,PROPFIND,VIEW");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+      });
+
     // GET
-    app.route ('/').get (controller.readall)
-    app.route ('/:param_id').get (controller.byid)
+    app.route ('/events').get (controller.readall)
+    app.route ('/events/:param_id').get (controller.byid)
     app.route ('/src/:lokasi').get (controller.search)
 
     // POST
-    app.route ('/').post (controller.plus)
+    app.route ('/events').post (controller.plus)
 
     // PATCH
-    app.route ('/:param_edit').patch (controller.edit)
+    app.route ('/events/:param_edit').patch (controller.edit)
 
     // DELETE
-    app.route ('/:param_kocok').delete (controller.erase)
+    app.route ('/events/:param_kocok').delete (controller.erase)
     
 }
